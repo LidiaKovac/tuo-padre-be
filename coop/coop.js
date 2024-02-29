15ea594bdee92d676ce635c555cadc6afce2343b
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2024-03-01 00:26:40 +0100

    fix coop

--- coop/coop.js
+++ coop/coop.js
@@ -11,74 +11,96 @@
 import fs from "fs";
 import moment from "moment";
 import puppeteer from "puppeteer";
-
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
 (async () => {
   try {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions("https://volantinocoop.it/", [
+      "geolocation",
+    ]);
     // Navigate the page to a URL
-    await page.goto(
-      "https://coop.volantinopiu.com/volantino1607800pv201.html?utm_source=coopliguria.volantinocoop.it&utm_medium=referral"
-    );
-
+    await page.goto("https://volantinocoop.it/cerca");
     // Set screen size
-    await page.setViewport({ width: 1495, height: 1024 });
+    // await page.setViewport({ width: 1495, height: 1024 });
+    await page.setGeolocation({ latitude: 44.414165, longitude: 8.942184 });
+    await page.type("#pac-input", "genova");
 
+    await delay(1000);
+    await page.waitForSelector("button#submit");
+    const sub = await page.$("button#submit");
+    await sub.scrollIntoView();
+    await sub.click();
+    console.log("Clicking");
+
+    // await delay(4000);
+    const shop = await page.waitForSelector(".list-menu .item");
+    await shop.click();
+    console.log("Clicking shop");
     // Type into search box
-    //   await page.type('.devsite-search-field', 'automate beyond recorder');
-
+    await delay(3000);
+    await page.reload();
     const cookie = await page.waitForSelector(
       "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"
     );
     if (cookie) {
-      cookie.click();
+      await cookie.click();
       console.log("Cookie accettati");
     }
-    setTimeout(async () => {
-      try {
-        // Wait and click on first result
-        const button = await page.waitForSelector(".stats.esplodi");
-        await button.click();
-        await page.screenshot({ path: "./image.png" });
-        console.log("Page opened, waiting for cards...");
-        await page.waitForSelector(".card");
-        const cards = await page.$$(".card");
-        const scadenza = await page.$eval(
-          ".barra_laterale .fw-semibold",
-          ({ innerText }) => innerText.split("al ")[1].trim()
-        );
-        const prodotti = [];
-        for (const card of cards) {
-          let img = null;
-          let price = null;
-          let prodName = null;
-          let prodQuantity = null;
-          let needsCard = false
-          img = await card.$eval("img", ({ src }) => src);
+    // Wait and click on first result
+    await delay(3000);
+    await page.screenshot({ path: "./image.png" });
+    console.log("Screenshot made");
+    const button = await page.waitForSelector(".esplodi", { visible: true });
+    await button.click();
+    console.log("Page opened, waiting for cards...");
+    await page.waitForSelector(".card");
+    console.log
+    const cards = await page.$$(".card");
+    const scadenza = await page.$eval(
+      ".barra_laterale .fw-semibold",
+      ({ innerText }) => innerText.split("al ")[1].trim()
+    );
+    const prodotti = [];
+    for (const card of cards) {
+      let img = null;
+      let price = null;
+      let prodName = null;
+      let prodQuantity = null;
+      let needsCard = false;
+      img = await card.$eval("img", ({ src }) => src);
 
-          const priceEl = await card.$(".product-price");
-          if (priceEl) {
-            price = await card.$eval(".product-price", (el) => el.innerText);
-          }
-          prodName = await card.$eval(
-            ".card-title",
-            ({ innerText }) => innerText
-          );
-          prodQuantity = await card.$eval(
-            ".card-text",
-            ({ innerText }) => innerText
-          );
-          needsCard = await card.$eval(".meccanica img", ({src}) => src.includes("soci"))
-          prodotti.push({ img, price, prodName, prodQuantity, store: "coop", needsCard, scadenza });
-        }
-        fs.writeFileSync("./coop.json", JSON.stringify(prodotti));
-        await browser.close();
-      } catch (error) {
-        console.log(error);
+      const priceEl = await card.$(".product-price");
+      if (priceEl) {
+        price = await card.$eval(".product-price", (el) => el.innerText);
       }
-    }, 3000);
+      prodName = await card.$eval(".card-title", ({ innerText }) => innerText);
+      prodQuantity = await card.$eval(
+        ".card-text",
+        ({ innerText }) => innerText
+      );
+      needsCard = await card.$eval(".meccanica img", ({ src }) =>
+        src.includes("soci")
+      );
+      prodotti.push({
+        img,
+        price,
+        prodName,
+        prodQuantity,
+        store: "coop",
+        needsCard,
+        scadenza,
+      });
+    }
+    fs.writeFileSync("./coop/coop.json", JSON.stringify(prodotti));
+    await browser.close();
   } catch (error) {
     console.log(error);
   }
---
+++ pam/pam.js
@@ -0,0 +1,107 @@
+// import  cheerio  from "cheerio"
+// import {parseFromString} from "dom-parser"
+// fetch("https://pam.volantinopiu.com/volantino1607800pv201.html?utm_source=pamliguria.volantinopam.it&utm_medium=referral")
+//     .then((res) => {
+//         return res.text()
+// }).then((volantino) => {
+//    const query =  cheerio.load(volantino)
+//    console.log(query(".stats.esplodi").click())
+// })
+
+import fs from "fs";
+import moment from "moment";
+import puppeteer from "puppeteer";
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+(async () => {
+  try {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Navigate the page to a URL
+    await page.goto("https://pam.volantinopiu.com/volantino1613700pv2295.html");
+
+    // Set screen size
+    await page.setViewport({ width: 1495, height: 1024 });
+    await delay(3000);
+    // Type into search box
+    //   await page.type('.devsite-search-field', 'automate beyond recorder');
+
+    // const cookie = await page.waitForSelector(
+    //   "#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"
+    // );
+    // if (cookie) {
+    //   cookie.click();
+    //   console.log("Cookie accettati");
+    // }
+    try {
+      // Wait and click on first result
+      const button = await page.waitForSelector(".stats.esplodi");
+      await button.click();
+      await page.screenshot({ path: "./image.png" });
+      console.log("Page opened, waiting for cards...");
+      await page.waitForSelector(".card");
+      const cards = await page.$$(".card");
+      const scadenza = await page.$eval(
+        ".barra_laterale .fw-semibold",
+        ({ innerText }) => innerText.split("al ")[1].trim()
+      );
+      const prodotti = [];
+      for (const card of cards) {
+        await delay(500);
+        let img = null;
+        let price = null;
+        let prodName = null;
+        let prodQuantity = null;
+        let needsCard = false;
+        img = await card.$eval("img", ({ src }) => src);
+
+        const priceEl = await card.$(".product-price");
+        if (priceEl) {
+          price = await card.$eval(".product-price", (el) => el.innerText);
+        } else continue;
+        prodName = await card.$eval(
+          ".card-title",
+          ({ innerText }) => innerText
+        );
+        prodQuantity = await card.$eval(
+          ".card-text",
+          ({ innerText }) => innerText
+        );
+        if (await card.$(".meccanica img")) {
+          needsCard = await card.$eval(".meccanica img", ({ src }) =>
+            src.includes("per_te")
+          );
+        } else needsCard = false;
+        console.log({
+          img,
+          price,
+          prodName,
+          prodQuantity,
+          store: "pam",
+          needsCard,
+          scadenza,
+        });
+        prodotti.push({
+          img,
+          price,
+          prodName,
+          prodQuantity,
+          store: "pam",
+          needsCard,
+          scadenza,
+        });
+      }
+      fs.writeFileSync("./pam/pam.json", JSON.stringify(prodotti));
+      await browser.close();
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})();
