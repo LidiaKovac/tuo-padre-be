b74dfb250e4430169bcd147b49757aba0341a166
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2024-03-07 00:48:22 +0100

    add bozza basko

---
+++ shops/basko/basko.js
@@ -0,0 +1,20 @@
+import { createWorker } from "tesseract.js";
+(async () => {
+  const worker = await createWorker("ita_old");
+  const ret = await worker.recognize("./shops/basko/part 2.png");
+  //   console.log(
+  //     ret.data.words
+  //       .map((b, i) => `i: ${i}: ${b.text}\n`)
+  //       .join("")
+  //   );
+
+  const price = ret.data.words[1].text;
+  const name = ret.data.words
+    .slice(7)
+    .map((w) => w.text)
+    .join(" ");
+  // remove stopwords?
+//   image will be the portion analyzed cut in the right bottom corner
+  console.log(price, name);
+  await worker.terminate();
+})();
