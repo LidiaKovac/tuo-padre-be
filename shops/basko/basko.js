001d0825776939250f98c9c23237964eb820ec43
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2024-03-08 01:30:38 +0100

    added cloudinary

--- shops/basko/basko.js
+++ shops/basko/basko.js
@@ -1,21 +1,34 @@
 // import { createWorker } from "tesseract.js";
 import { createWorker } from "tesseract.js";
 import sharp from "sharp";
+import { v2 as cloudinary } from "cloudinary";
 import { readFileSync, writeFileSync } from "fs";
+import { readdir, unlink } from "fs/promises";
+const { CLOUDINARY_URL } = process.env;
+
+import "dotenv/config";
 // this is important!
 (async () => {
   const worker = await createWorker("ita_old");
+  cloudinary.config({
+    cloud_name: "dhbeeld3u",
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
+  console.log("Deleting files on cloudinary...");
+  await cloudinary.api.delete_resources_by_prefix("shopping");
+
   const width = 265;
   const height = 320;
   for (let i = 0; i < 4; i++) {
     for (let y = 0; y < 3; y++) {
       const startW = 20 + width * i;
       const startH = 120 + height * y;
-      console.log(startW, startH);
       await sharp("./shops/basko/3.png")
-      .greyscale()
-      .threshold()
-      .normalise()
+        .greyscale()
+        .threshold()
+        .normalise()
         .extract({
           left: startW,
           top: startH,
@@ -25,29 +38,32 @@ import { readFileSync, writeFileSync } from "fs";
         .toFile(`./shops/basko/parts/cut${i}-${y}.jpg`);
     }
   }
-  for (let i = 0; i < 4; i++) {
-    for (let y = 0; y < 3; y++) {
-      const alreadyIn = JSON.parse(
-        readFileSync("./shops/basko/basko.json", "utf-8")
-      );
+  const images = [];
+  const files = await readdir("./shops/basko/parts/");
+  console.log("Uploading files...");
+  for (const file of files) {
+    const r = await cloudinary.uploader.upload(`./shops/basko/parts/${file}`, {
+      folder: "shopping",
+    });
+    images.push(r.secure_url);
+    await unlink(`./shops/basko/parts/${file}`);
+    console.log("✅ Uploaded!");
+  }
+  console.log("Local files cleaned up, writing on JSON file");
+  for (const img of images) {
+    const alreadyIn = JSON.parse(
+      readFileSync("./shops/basko/basko.json", "utf-8")
+    );
 
-      const ret = await worker.recognize(`./shops/basko/parts/cut${i}-${y}.jpg`);
-      //   console.log(
-      //     ret.data.words
-      //       .map((b, i) => `i: ${i}: ${b.text}\n`)
-      //       .join("")
-      //   );
-      const name = ret.data.words.map((w) => w.text).join(" ");
-      // remove stopwords?
-      //   image will be the portion analyzed cut in the right bottom corner
-      const final = {
-        store: "basko",
-        img: `./shops/basko/cut${i}-${y}.jpg`, //needs cloudinary
-        name,
-      };
-      alreadyIn.push(final);
-      writeFileSync("./shops/basko/basko.json", JSON.stringify(alreadyIn));
-    }
+    const ret = await worker.recognize(img);
+    const name = ret.data.words.map((w) => w.text).join(" ");
+    const final = {
+      store: "basko",
+      img,
+      name,
+    };
+    alreadyIn.push(final);
+    writeFileSync("./shops/basko/basko.json", JSON.stringify(alreadyIn));
   }
   await worker.terminate();
 })();
