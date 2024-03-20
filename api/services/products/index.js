0b251efd6d24795bb4df15b9c6f73a5c4f2a4bfe
Lidia Kovac <lidia.kovac1998@gmail.com>
2024-03-20 21:37:11 +0100

    add query

--- api/services/products/index.js
+++ api/services/products/index.js
@@ -2,36 +2,32 @@ import { Router } from "express"
 import Product from "../../schemas/product.schema.js"
 // import { db } from "../../index.js";
 import q2m from "query-to-mongo"
+import { StemmerIt } from "@nlpjs/lang-it"
+import { Logger } from "../../../shops/logger.js"
 const prodRoute = Router()
 
 prodRoute.get("/", async (req, res, next) => {
-  /* 
-        Available params: 
-            - page (int), default = 1
-            - size (int), default = 20
-    */
-  const query = q2m(req.url.split("?")[1])
-  let { page, size } = req.query
+  let { page, size, store: stores } = req.query
+  const query = q2m(req.url.split("?")[1], { ignore: "store" })
+  const storeQ = { $or: [] }
+  for (const store of stores.split(",")) {
+    if (store.length > 0) {
+      storeQ.$or.push({
+        store: store.toLowerCase().replaceAll(" ", "-")
+      })
+    }
+  }
   if (!page) page = 1
   if (!size) size = 20
-  const q = {
+  const nlp = new StemmerIt()
+  const commonQ = {
     ...query.criteria,
-    prodName: query.criteria.prodName ? { $regex: query.criteria.prodName } : "",
-
+    ...storeQ,
+    prodName: (query.criteria.prodName && query.criteria.prodName.length > 0) ? { $regex: new RegExp(`${nlp.stemWord(query.criteria.prodName)}`, "i") } : undefined,
   }
-  console.log(query)
-  const count = await Product.find({
-    ...q,
-    // price: {
-    //   $exists: true
-    // }
-  }).count()
-  const prods = await Product.find(q, null, {
-    limit: size,
-    skip: size * page - 1,
-  })
-  console.log(prods)
-  // if (query) res.send(db.findByName(query, page, size))
+  Logger.debug(commonQ)
+  const count = await Product.find((query.criteria.prodName && query.criteria.prodName.length > 0) ? commonQ : { ...storeQ, ...query.criteria }).count()
+  const prods = await Product.find((query.criteria.prodName && query.criteria.prodName.length > 0) ? commonQ : { ...storeQ, ...query.criteria }).limit(size).skip(size * (page - 1))
   res.send({
     data: prods,
     count,
